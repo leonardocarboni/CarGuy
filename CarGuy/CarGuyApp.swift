ff560439cf1a9a98b5e95c0bb6bdff7d5439aa47
Leonardo Carboni <43882851+leonardocarboni@users.noreply.github.com>
2022-05-17 14:17:50 +0200

    Initial Commit

---
+++ CarGuy/CarGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarGuyApp.swift
+//  CarGuy
+//
+//  Created by Leonardo Carboni on 17/05/22.
+//
+
+import SwiftUI
+
+@main
+struct CarGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
